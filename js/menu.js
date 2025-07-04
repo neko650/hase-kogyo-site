fbed3c85d0fe3c49c3a03215108b0f14107bc50f
neko650 <jjj123588@gmail.com>
2025-07-05 08:44:08 +0900

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger");
-  const navMenu = document.querySelector(".nav-menu");
 
-  hamburger.addEventListener("click", function () {
-    navMenu.classList.toggle("active");
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".nav-menu");
+  const hamburger = document.querySelector(".hamburger");
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
   });
 
-  document.querySelectorAll(".nav-menu a").forEach(link => {
-    link.addEventListener("click", function (e) {
-      if (!link.href.includes("news.html")) {
-        navMenu.classList.remove("active");
+  const fadeIns = document.querySelectorAll(".fade-in, .gallery-item");
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
       }
     });
   });
+  fadeIns.forEach(el => observer.observe(el));
 });
